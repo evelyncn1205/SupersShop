f8c1259ae01c06ba4dfcebaa0727210b36adb4d3
Evelyncn1205 <evelynrx_rj@hotmail.com>
2023-05-18 15:22:33 +0100

    First Controller

---
+++ SupersShop/Data/Entities/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Permissions;
+
+namespace SuperShop.Data.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [DisplayFormat(DataFormatString ="{0:C2}", ApplyFormatInEditMode = false)] // formata em duas casas decimais quando for moeda
+        public decimal Price { get; set; }
+
+        [Display(Name ="Image")]
+        public string ImageUrl { get; set; }
+
+        [Display(Name ="Last Purchase")]
+        public DateTime LastPurchase { get; set; }
+
+        [Display(Name = "Last Sale")]
+        public DateTime LastSale { get; set; }
+
+        [Display(Name = "Is Availabe")]
+        public bool IsAvailable { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
+        public double Stpck { get; set; }
+    }
+}
